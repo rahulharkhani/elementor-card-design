94c97368f04a4e4bacc6833a5ce2cf7041bdc958
rahul.l.harkhani <rahul.l.harkhani@doyenhub.com>
2022-01-20 23:10:16 +0530

    fix icon issue

--- elementor-card-design.php
+++ elementor-card-design.php
@@ -179,7 +179,8 @@ if (!function_exists('elementor_card_design_installed')) {
  */
 function card_design_fontawesome_cdn() {
     ?>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"> -->
     <?php
 }
 add_action('wp_head', 'card_design_fontawesome_cdn');
